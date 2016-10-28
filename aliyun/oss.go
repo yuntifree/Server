edd5a732b18ca7f6990599a9dbdefb98472e525a
alanrong <alanrong@gmail.com>
2016-10-28 17:24:57 +0800

    add aliyun

---
+++ aliyun/oss.go
@@ -0,0 +1,38 @@
+package aliyun
+
+import (
+	"log"
+	"strings"
+
+	oss "github.com/aliyun/aliyun-oss-go-sdk/oss"
+)
+
+const (
+	endpoint        = "oss-cn-shenzhen.aliyuncs.com"
+	accessKeyID     = "LTAIOpvgiTmAKJNi"
+	accessKeySecret = "apT9ttTZcedRj5bPdOlmLgvT8vM4R4"
+	yuntiBucket     = "yuntinews"
+)
+
+//UploadOssFile upload content to aliyun oss
+func UploadOssFile(filename, content string) bool {
+	client, err := oss.New(endpoint, accessKeyID, accessKeySecret)
+	if err != nil {
+		log.Printf("oss init failed:%v", err)
+		return false
+	}
+
+	bucket, err := client.Bucket(yuntiBucket)
+	if err != nil {
+		log.Printf("bucket init failed:%v", err)
+		return false
+	}
+
+	err = bucket.PutObject(filename, strings.NewReader(content))
+	if err != nil {
+		log.Printf("PutObject failed %s: %v", filename, err)
+		return false
+	}
+
+	return true
+}
