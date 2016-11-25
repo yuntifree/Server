dd86c5f4c17fb5c6800decfd45b4230b809e7437
alanrong <alanrong@gmail.com>
2016-11-25 17:38:58 +0800

    add SendAlertMail

---
+++ util/mail.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+const (
+	mailAddress = "dingrong@yunxingzh.com"
+	mailPasswd  = "Yunti2016"
+	smtpAddress = "smtp.yunxingzh.com"
+)
+
+//SendAlertMail send alert mail
+func SendAlertMail(content string) error {
+	auth := smtp.PlainAuth(
+		"",
+		mailAddress,
+		mailPasswd,
+		smtpAddress,
+	)
+	var mailList []string
+	mailList = append(mailList, mailAddress)
+	subject := "【告警邮件】"
+	sub := fmt.Sprintf("subject: %s\r\n\r\n", subject)
+
+	err := smtp.SendMail(
+		smtpAddress+":25",
+		auth,
+		mailAddress,
+		mailList,
+		[]byte(sub+content),
+	)
+	return err
+}
