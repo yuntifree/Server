4f78d61329d028eabd938e7d1d55df5659f315d5
alanrong <alanrong@gmail.com>
2016-12-08 15:27:47 +0800

    add https

--- access/app.go
+++ access/app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/tls"
+	"log"
 	"net/http"
 
 	"../httpserver"
@@ -8,8 +10,20 @@ import (
 	"github.com/facebookgo/grace/gracehttp"
 )
 
+const (
+	certPath    = "/etc/letsencrypt/live/yunxingzh.com/fullchain.pem"
+	privKeyPath = "/etc/letsencrypt/live/yunxingzh.com/privkey.pem"
+)
+
 func main() {
+	cer, err := tls.LoadX509KeyPair(certPath, privKeyPath)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	config := &tls.Config{Certificates: []tls.Certificate{cer}}
 	gracehttp.Serve(
 		&http.Server{Addr: ":80", Handler: httpserver.NewAppServer()},
+		&http.Server{Addr: ":443", Handler: httpserver.NewAppServer(), TLSConfig: config},
 	)
 }
