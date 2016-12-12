2a4fd2f93be9839c7d0419e0f453d99613f634aa
alanrong <alanrong@gmail.com>
2016-12-12 14:56:26 +0800

    for special cases

--- tools/loadaddress.go
+++ tools/loadaddress.go
@@ -45,5 +45,13 @@ func main() {
 			log.Printf("insert zipcode failed:%d %s %v", code, address, err)
 		}
 	}
+	db.Exec("INSERT INTO zipcode(code, address) VALUES(110100, '北京市辖区')")
+	db.Exec("INSERT INTO zipcode(code, address) VALUES(110200, '北京市辖县')")
+	db.Exec("INSERT INTO zipcode(code, address) VALUES(120100, '天津市辖区')")
+	db.Exec("INSERT INTO zipcode(code, address) VALUES(120200, '天津市辖县')")
+	db.Exec("INSERT INTO zipcode(code, address) VALUES(310100, '上海市辖区')")
+	db.Exec("INSERT INTO zipcode(code, address) VALUES(310200, '上海市辖县')")
+	db.Exec("INSERT INTO zipcode(code, address) VALUES(500100, '重庆市辖区')")
+	db.Exec("INSERT INTO zipcode(code, address) VALUES(500200, '重庆市辖县')")
 
 }
