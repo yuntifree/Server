f534da0bede87ec4648c0f8e10c35de085df8906
alanrong <alanrong@gmail.com>
2017-02-09 16:21:04 +0800

    update renew

--- shell/renew_cert.sh
+++ shell/renew_cert.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-cd /data/darren/certbot
-./certbot-auto renew
-
 IPLIST="10.27.178.90 10.27.168.11"
 SERVER=/data/server/appserver
+
+#for wx.yunxingzh.com
+cd /data/darren/certbot
+./certbot-auto renew
+cp -f /etc/letsencrypt/live/wx.yunxingzh.com/fullchain.pem /data/server
+cp -f /etc/letsencrypt/live/wx.yunxingzh.com/privkey.pem /data/server
+ps -ef|grep $SERVER |grep -v grep|gawk -e '{print $2}'|xargs kill -12
+
+#for api.yunxingzh.com
+ssh root@10.27.178.90 "/data/certbot/certbot-auto renew"
+scp  root@10.27.178.90:/etc/letsencrypt/live/api.yunxingzh.com/fullchain.pem /tmp/
+scp  root@10.27.178.90:/etc/letsencrypt/live/api.yunxingzh.com/privkey.pem /tmp/
 for ip in $IPLIST; do
-    scp /etc/letsencrypt/live/yunxingzh.com/* root@$ip:/etc/letsencrypt/live/yunxingzh.com/
-    ssh root@$ip "ps -ef|grep $SERVER |grep -v grep|gawk -e '{print \$2}'|xargs kill -SIGUSR2"
+    scp  /tmp/*.pem root@$ip:/data/server
+    ssh root@$ip "ps -ef|grep $SERVER |grep -v grep|gawk -e '{print $2}'|xargs kill -12"
     echo "restart $ip appserver..."
 done
 
