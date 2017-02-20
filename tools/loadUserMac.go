fd14b94ae672d8410365a3e0d42941f745dbfb65
alanrong <alanrong@gmail.com>
2017-02-20 08:54:59 +0800

    load user mac from csv

--- tools/loadUserMac.go
+++ tools/loadUserMac.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"Server/util"
+	"bufio"
 	"database/sql"
 	"log"
 	"os"
+	"strings"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -14,7 +17,7 @@ var db *sql.DB
 func recordUserMac(db *sql.DB, phone, mac string, stype uint) {
 	token := util.GenSalt()
 	privdata := util.GenSalt()
-	res, err := db.Exec("INSERT IGNORE INTO user(username, phone, token, private, term, ctime, atime, etime, bitmap) VALUES (?, ?, ?, ?, 2, NOW(), NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY), ?)", phone, phone, token, privdata, 1<<stype)
+	res, err := db.Exec("INSERT INTO user(username, phone, token, private, term, ctime, atime, etime, bitmap) VALUES (?, ?, ?, ?, 2, NOW(), NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY), ?) ON DUPLICATE KEY UPDATE bitmap = bitmap | ?", phone, phone, token, privdata, 1<<stype, 1<<stype)
 	if err != nil {
 		log.Printf("recordUserMac create user failed:%v", err)
 		return
@@ -23,6 +26,15 @@ func recordUserMac(db *sql.DB, phone, mac string, stype uint) {
 	if err != nil {
 		log.Printf("recordUserMac get user id failed:%v", err)
 		return
+	}
+	if uid == 0 {
+		err := db.QueryRow("SELECT uid FROM user WHERE username = ?", phone).Scan(&uid)
+		if err != nil {
+			log.Printf("recordUserMac get user id failed uid = 0, phone:%s mac:%s",
+				phone, mac)
+			return
+
+		}
 	}
 	_, err = db.Exec("INSERT INTO user_mac(uid, phone, mac, ctime, etime) VALUES (?, ?, ?, NOW(), DATE_ADD(NOW(), INTERVAL 30 DAY)) ON DUPLICATE KEY UPDATE uid = ?, phone = ?, etime = DATE_ADD(NOW(), INTERVAL 30 DAY)", uid, phone, mac, uid, phone)
 	if err != nil {
@@ -38,9 +50,34 @@ func main() {
 		log.Printf("InitDB failed:%v", err)
 		os.Exit(1)
 	}
-	phone := "18219201566"
-	mac := "945330575f35"
-	var stype uint
-	stype = 1
-	recordUserMac(db, phone, mac, stype)
+	f, err := os.Open("wg_phone_mac.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	rd := bufio.NewReader(f)
+	var idx int64
+	for {
+		idx++
+		if idx%1000 == 0 {
+			time.Sleep(1 * time.Second)
+		}
+		data, err := rd.ReadSlice('\n')
+		if err != nil {
+			break
+		}
+		line := string(data)
+		arr := strings.Split(line, ";")
+		phone := arr[0]
+		mac := arr[1]
+		phone = strings.TrimSpace(phone)
+		mac = strings.TrimSpace(mac)
+		if phone == "" || mac == "" {
+			continue
+		}
+		log.Printf("phone:%s mac:%s", phone, mac)
+		recordUserMac(db, phone, mac, 1)
+	}
+	return
 }
