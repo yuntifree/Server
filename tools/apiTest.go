39b04177fde0ab2b6b671341c65750e0a83f07a0
alanrong <alanrong@gmail.com>
2017-03-03 15:08:19 +0800

    add apiTest for simple api test

---
+++ tools/apiTest.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"Server/util"
+	"log"
+	"os"
+
+	simplejson "github.com/bitly/go-simplejson"
+)
+
+const (
+	baseURL = "http://10.26.210.175/"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Printf("not enough param")
+		os.Exit(1)
+	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	js, err := simplejson.NewFromReader(f)
+	if err != nil {
+		panic(err)
+	}
+
+	arr := js.Get("api").MustArray()
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(arr); i++ {
+		name, err := js.Get("api").GetIndex(i).Get("name").String()
+		req := js.Get("api").GetIndex(i).Get("req")
+		log.Printf("name:%s", name)
+		if err != nil {
+			panic(err)
+		}
+		body, err := req.Encode()
+		if err != nil {
+			log.Printf("encode failed:%s %v", name, err)
+			continue
+		}
+		url := baseURL + name
+		resp, err := util.HTTPRequest(url, string(body))
+		if err != nil {
+			log.Printf("Test Failed:%s %v", name, err)
+			continue
+		}
+		rs, err := simplejson.NewJson([]byte(resp))
+		if err != nil {
+			log.Printf("parse response failed:%s %v", name, err)
+			continue
+		}
+		errno, err := rs.Get("errno").Int64()
+		if err != nil {
+			log.Printf("get errno failed:%s %v", name, err)
+			continue
+		}
+		if errno != 0 {
+			log.Printf("API TEST FAILED:%s ERRNO:%d", name, errno)
+		}
+	}
+	return
+}
