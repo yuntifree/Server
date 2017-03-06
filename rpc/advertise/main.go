5c53c99108d069a31bbad24291cc35df584f92fb
alanrong <alanrong@gmail.com>
2017-03-06 21:02:05 +0800

    add get_customer

--- rpc/advertise/main.go
+++ rpc/advertise/main.go
@@ -71,6 +71,52 @@ func (s *server) ModCustomer(ctx context.Context, in *advertise.CustomerRequest)
 		Head: &common.Head{Retcode: 0}}, nil
 }
 
+func convertCustomerInfo(info Customer) advertise.CustomerInfo {
+	return advertise.CustomerInfo{
+		Id:      info.ID,
+		Name:    info.Name,
+		Contact: info.Contact,
+		Phone:   info.Phone,
+		Address: info.Address,
+		Remark:  info.Remark,
+		Deleted: info.Deleted,
+		Atime:   info.Atime,
+		Etime:   info.Etime,
+	}
+}
+
+func fetchCustomer(db *gorm.DB, seq, num int64) []*advertise.CustomerInfo {
+	var infos []*advertise.CustomerInfo
+	var customers []Customer
+	if seq == 0 {
+		db.Where("deleted = 0").Order("id desc").Limit(num).Find(&customers)
+	} else {
+		db.Where("deleted = 0 AND id < ?", seq).Order("id desc").Limit(num).Find(&customers)
+	}
+	if len(customers) > 0 {
+		for i := 0; i < len(customers); i++ {
+			info := convertCustomerInfo(customers[i])
+			infos = append(infos, &info)
+		}
+	}
+	return infos
+}
+
+func getTotalCustomer(db *gorm.DB) int64 {
+	var count int64
+	db.Model(&Customer{}).Where("deleted = 0").Count(&count)
+	return count
+}
+
+func (s *server) FetchCustomer(ctx context.Context, in *common.CommRequest) (*advertise.CustomerReply, error) {
+	util.PubRPCRequest(w, "advertise", "FetchCustomer")
+	infos := fetchCustomer(db, in.Seq, in.Num)
+	total := getTotalCustomer(db)
+	util.PubRPCSuccRsp(w, "advertise", "FetchCustomer")
+	return &advertise.CustomerReply{
+		Head: &common.Head{Retcode: 0}, Infos: infos, Total: total}, nil
+}
+
 func main() {
 	lis, err := net.Listen("tcp", util.AdvertiseServerPort)
 	if err != nil {
