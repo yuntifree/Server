6190103c892e7243417428adc7545afe33ee6282
hzgnpu <tinylee@qq.com>
2017-04-25 17:46:05 +0800

    modify wx

--- tools/wxrand.go
+++ tools/wxrand.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
@@ -13,9 +15,9 @@ import (
 )
 
 const (
-	baseUrl    = "http://www.gsdata.cn/rank/single?id="
-	contentUrl = "http://www.gsdata.cn/rank/recommendArticles?type=hot&id="
-	imgUrl     = "http://img1.gsdata.cn/index.php/rank/getImageUrl?hash="
+	baseUrl    = "http://www.gsdata.cn/rank/wxdetail?wxname="
+	contentUrl = "http://www.gsdata.cn/rank/toparc?sort=-1&wxname="
+	imgUrl     = "http://img1.gsdata.cn/index.php/rank/getimageurl?callback=_&hash="
 )
 
 //WxData information for wxData
@@ -35,34 +37,17 @@ type WxContent struct {
 	HomeUrl string // 公众号主页，点击进入公众号用
 }
 
-var ids = [][2]string{
-	{"MxTuEex5N2T1Mtxa", "111531"},                  // OurDongguan
-	{"MxTuQe25N2D1ctyaNnggO0O0O#O0O0O4", "1464726"}, // 东莞大喇叭
-	{"MxTuYew5M2z1Qt5aNnwgO0O0O#O0O0O4", "1603497"}, // 东莞美食先锋队
-	{"MxTuAe35O2T1ktxa", "107991"},                  // 东莞时间网
-	{"MxTuUez5N2T1YtxaNnAgO0O0O#O0O0O4", "1535614"}, // 东莞生活君
-	{"MxTuMe35M2z1ctza", "137373"},                  // 东莞美食大搜罗
-	{"MxTuAe35O2T1gtxa", "107981"},                  // 东莞阳光网
-	{"MxTuIe45M2j1gtO0O0Oa", "12828"},               // 东莞日报
-	{"MxTuEex5N2T1Mtwa", "111530"},                  // 流行东莞
-	{"MxTuUez5M2j1EtzaNnQgO0O0O#O0O0O4", "1532135"}, // 差评
-	{"Mxjuce55O2A1O0O0OtO0O0Oa", "2798"},            // 虎嗅网
-	{"MxTuUe05O2D1QtyaMnwgO0O0O#O0O0O4", "1548423"}, // 超高能E姐
-	{"MxjuceO0O0O5", "27"},                          // Vista看天下
-	{"MxzuYe35O2D1Ut5a", "367859"},                  // 同道大叔
-	{"MxTuAe35N2T1Ytya", "107562"},                  // 人民日报
-	{"MxTuce25M2z1gtO0O0Oa", "17638"},               // 深夜发媸
-	{"NxTuQez5N2T1Itwa", "543520"},                  // 二更食堂
-	{"MxTuQez5", "143"},                             // 十点读书
+var ids = []string{
+	"mimeng7",
 }
 
 var limit chan int
 
-func getWxInfo(id [2]string, ch chan string) WxInfo {
+func getWxInfo(id string, ch chan string) WxInfo {
 	limit <- 1
 	var wxData WxInfo
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", baseUrl+id[0], nil)
+	req, err := http.NewRequest("GET", baseUrl+id, nil)
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36")
 	resp, err := client.Do(req)
 	defer resp.Body.Close()
@@ -73,14 +58,14 @@ func getWxInfo(id [2]string, ch chan string) WxInfo {
 		return wxData
 	}
 
-	div := d.Find(".number-txt")
-	wxData.Id = div.Find("#wx_name").Text()
-	wxData.Name = d.Find(".number-title #wx_nickname").Text()
-	wxData.Desc = div.Find(".wx-sp").Eq(1).Find(".sp-txt").Text()
+	div := d.Find(".wxDetail-top")
+	wxData.Id = id
+	wxData.Name = d.Find(".wxDetail-name label").Text()
+	wxData.Desc = div.Find(".info-li p").Last().Text()
 	wxData.HeadUrl = "http://open.weixin.qq.com/qr/code/?username=" + wxData.Id
 
 	<-limit
-	ch <- fmtContent(wxData, getWxContent(id[1]))
+	ch <- fmtContent(wxData, getWxContent(id))
 
 	return wxData
 }
@@ -104,19 +89,19 @@ func getWxContent(id string) []WxContent {
 		return nil
 	}
 
-	ret, _ := js.Get("status").String()
-	if ret != "OK" {
+	ret, _ := js.Get("error").Int()
+	if ret != 0 {
 		log.Printf("status not ok: %v", ret)
 		return nil
 	}
 	// get article and images, 取得最近一篇文章
 	// TODO: check empty items
-	artlist, _ := js.Get("result").Get("items").Array()
+	artlist, _ := js.Get("data").Array()
 	artlen := len(artlist)
 	items := make([]WxContent, artlen)
 
 	for i := 0; i < artlen; i++ {
-		article := js.Get("result").Get("items").GetIndex(i)
+		article := js.Get("data").GetIndex(i)
 		// post infos
 		items[i].PostTime, _ = article.Get("posttime").String()
 		postUrl, _ := article.Get("url").String()
@@ -131,9 +116,9 @@ func getWxContent(id string) []WxContent {
 		items[i].Desc, _ = article.Get("content").String()
 
 		// fetch aliyun imageurl
-		picurl, _ := article.Get("picurl").String()
+		picurl, _ := article.Get("data-hash").String()
 
-		items[i].Image = getImageUrl(picurl)
+		items[i].Image = getImageUrl(url.QueryEscape(picurl))
 	}
 
 	return items
@@ -151,7 +136,12 @@ func getImageUrl(qurl string) string {
 	resp, err := client.Do(req)
 	defer resp.Body.Close()
 
-	js, err := simplejson.NewFromReader(resp.Body)
+	str, err := ioutil.ReadAll(resp.Body)
+	if checkerr(err) {
+		return ""
+	}
+
+	js, err := simplejson.NewJson([]byte(str[2 : len(str)-1]))
 	if err != nil {
 		log.Printf("parse url json failed:%v", err)
 		return ""
@@ -162,10 +152,12 @@ func getImageUrl(qurl string) string {
 	return ret
 }
 
-func checkerr(e error) {
+func checkerr(e error) bool {
 	if e != nil {
 		panic(e)
+		return true
 	}
+	return false
 }
 
 func fmtHeader() string {
