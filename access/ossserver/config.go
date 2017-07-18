dc4b17f3b2d43081ab8937bf257ed488be036426
alanrong <alanrong@gmail.com>
2017-07-18 16:34:07 +0800

    add add_login_img and mod_login_img

--- access/ossserver/config.go
+++ access/ossserver/config.go
@@ -25,6 +25,10 @@ func configHandler(w http.ResponseWriter, r *http.Request) (apperr *util.AppErro
 	switch action {
 	case "get_login_img":
 		getLoginImg(w, r)
+	case "add_login_img":
+		addLoginImg(w, r)
+	case "mod_login_img":
+		modLoginImg(w, r)
 	default:
 		panic(util.AppError{101, "unknown action", ""})
 	}
@@ -46,6 +50,60 @@ func getLoginImg(w http.ResponseWriter, r *http.Request) {
 	httpserver.CheckRPCErr(rpcerr, "GetLoginImg")
 	res := resp.Interface().(*config.LoginImgReply)
 	httpserver.CheckRPCCode(res.Head.Retcode, "GetLoginImg")
+
+	body := httpserver.GenResponseBody(res, false)
+	w.Write(body)
+	httpserver.ReportSuccResp(r.RequestURI)
+}
+
+func addLoginImg(w http.ResponseWriter, r *http.Request) {
+	var req httpserver.Request
+	req.InitCheckOss(r)
+	uid := req.GetParamInt("uid")
+	stype := req.GetParamInt("type")
+	stime := req.GetParamIntDef("stime", 0)
+	etime := req.GetParamIntDef("etime", 0)
+	img := req.GetParamString("img")
+
+	uuid := util.GenUUID()
+	resp, rpcerr := httpserver.CallRPC(util.ConfigServerType,
+		uid, "AddLoginImg",
+		&config.LoginImgRequest{
+			Head: &common.Head{Sid: uuid},
+			Info: &config.LoginImgInfo{Type: stype, Stime: stime,
+				Etime: etime, Img: img}})
+	httpserver.CheckRPCErr(rpcerr, "AddLoginImg")
+	res := resp.Interface().(*common.CommReply)
+	httpserver.CheckRPCCode(res.Head.Retcode, "AddLoginImg")
+
+	body := httpserver.GenResponseBody(res, false)
+	w.Write(body)
+	httpserver.ReportSuccResp(r.RequestURI)
+}
+
+func modLoginImg(w http.ResponseWriter, r *http.Request) {
+	var req httpserver.Request
+	req.InitCheckOss(r)
+	uid := req.GetParamInt("uid")
+	id := req.GetParamInt("id")
+	stype := req.GetParamInt("type")
+	stime := req.GetParamIntDef("stime", 0)
+	etime := req.GetParamIntDef("etime", 0)
+	img := req.GetParamString("img")
+	online := req.GetParamIntDef("online", 0)
+	deleted := req.GetParamIntDef("deleted", 0)
+
+	uuid := util.GenUUID()
+	resp, rpcerr := httpserver.CallRPC(util.ConfigServerType,
+		uid, "ModLoginImg",
+		&config.LoginImgRequest{
+			Head: &common.Head{Sid: uuid},
+			Info: &config.LoginImgInfo{Id: id, Type: stype,
+				Stime: stime, Etime: etime, Img: img,
+				Online: online, Deleted: deleted}})
+	httpserver.CheckRPCErr(rpcerr, "ModLoginImg")
+	res := resp.Interface().(*common.CommReply)
+	httpserver.CheckRPCCode(res.Head.Retcode, "ModLoginImg")
 
 	body := httpserver.GenResponseBody(res, false)
 	w.Write(body)
--- rpc/config/loginimg.go
+++ rpc/config/loginimg.go
@@ -39,6 +39,15 @@ func getLoginImg(db *sql.DB, stype, seq, num int64) []*config.LoginImgInfo {
 	return infos
 }
 
+func getTotalLoginImg(db *sql.DB, stype int64) int64 {
+	var cnt int64
+	err := db.QueryRow("SELECT COUNT(id) FROM login_banner WHERE type = ? AND deleted = 0", stype).Scan(&cnt)
+	if err != nil {
+		log.Printf("getTotalLoginImg failed:%v", err)
+	}
+	return cnt
+}
+
 func (s *server) GetLoginImg(ctx context.Context, in *common.CommRequest) (*config.LoginImgReply, error) {
 	util.PubRPCRequest(w, "config", "GetLoginImg")
 	infos := getLoginImg(db, in.Type, in.Seq, in.Num)
@@ -46,8 +55,62 @@ func (s *server) GetLoginImg(ctx context.Context, in *common.CommRequest) (*conf
 	if len(infos) >= int(in.Num) {
 		hasmore = 1
 	}
+	total := getTotalLoginImg(db, in.Type)
 	util.PubRPCSuccRsp(w, "config", "GetLoginImg")
 	return &config.LoginImgReply{
 		Head:  &common.Head{Retcode: 0, Uid: in.Head.Uid},
-		Infos: infos, Hasmore: hasmore}, nil
+		Infos: infos, Hasmore: hasmore, Total: total}, nil
+}
+
+func addLoginImg(db *sql.DB, info *config.LoginImgInfo) (int64, error) {
+	res, err := db.Exec("INSERT INTO login_banner(type, img, stime, etime, ctime) VALUES (?, ?, ?, ?, NOW())",
+		info.Type, info.Img, info.Stime, info.Etime)
+	if err != nil {
+		log.Printf("addLoginImg failed:%+v %v", info, err)
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Printf("addLoginImg get insert id failed:%+v %v", info, err)
+		return 0, err
+	}
+	return id, nil
+}
+
+func (s *server) AddLoginImg(ctx context.Context, in *config.LoginImgRequest) (*common.CommReply, error) {
+	util.PubRPCRequest(w, "config", "AddLoginImg")
+	id, err := addLoginImg(db, in.Info)
+	if err != nil {
+		log.Printf("addLoginImg failed:%v", err)
+		return &common.CommReply{
+			Head: &common.Head{Retcode: 1, Uid: in.Head.Uid}}, nil
+	}
+	util.PubRPCSuccRsp(w, "config", "AddLoginImg")
+	return &common.CommReply{
+		Head: &common.Head{Retcode: 0, Uid: in.Head.Uid},
+		Id:   id}, nil
+}
+
+func modLoginImg(db *sql.DB, info *config.LoginImgInfo) error {
+	_, err := db.Exec("UPDATE login_banner SET img = ?, stime = ?, etime = ?, online = ?, deleted = ? WHERE id = ?",
+		info.Img, info.Stime, info.Etime, info.Online, info.Deleted,
+		info.Id)
+	if err != nil {
+		log.Printf("addLoginImg failed:%+v %v", info, err)
+		return err
+	}
+	return nil
+}
+
+func (s *server) ModLoginImg(ctx context.Context, in *config.LoginImgRequest) (*common.CommReply, error) {
+	util.PubRPCRequest(w, "config", "ModLoginImg")
+	err := modLoginImg(db, in.Info)
+	if err != nil {
+		log.Printf("modLoginImg failed:%v", err)
+		return &common.CommReply{
+			Head: &common.Head{Retcode: 1, Uid: in.Head.Uid}}, nil
+	}
+	util.PubRPCSuccRsp(w, "config", "ModLoginImg")
+	return &common.CommReply{
+		Head: &common.Head{Retcode: 0, Uid: in.Head.Uid}}, nil
 }
