2997f7e19ed84e9fa28ce5117899f7ff20d0c9c0
alanrong <alanrong@gmail.com>
2017-09-21 10:43:38 +0800

    for spider

--- db/weixin.sql
+++ db/weixin.sql
@@ -22,3 +22,53 @@ CREATE TABLE IF NOT EXISTS gzh_article
     PRIMARY KEY(id),
     UNIQUE KEY(wid, md5)
 ) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS weixin
+(
+    id  bigint unsigned NOT NULL AUTO_INCREMENT,
+    biz varchar(255) NOT NULL DEFAULT '',
+    collect int unsigned NOT NULL DEFAULT 0 COMMENT '采集时间戳',
+    ctime   datetime NOT NULL DEFAULT '2016-01-01 00:00:00',
+    PRIMARY KEY(id),
+    UNIQUE KEY(biz)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS post 
+(
+    id  bigint unsigned NOT NULL AUTO_INCREMENT,
+    biz varchar(255) NOT NULL DEFAULT '',
+    field_id    int unsigned NOT NULL DEFAULT 0,
+    title   varchar(255) NOT NULL DEFAULT '',
+    title_encode text NOT NULL DEFAULT '',
+    digest varchar(500) NOT NULL DEFAULT '',
+    content_url varchar(500) NOT NULL DEFAULT '',
+    source_url varchar(500) NOT NULL DEFAULT '',
+    cover varchar(500) NOT NULL DEFAULT '',
+    is_multi tinyint unsigned NOT NULL DEFAULT 0 COMMENT '是否多图文',
+    is_top tinyint unsigned NOT NULL DEFAULT 0 COMMENT '是否头条',
+    datetime int unsigned NOT NULL DEFAULT 0 COMMENT '文章时间戳',
+    read_num int unsigned NOT NULL DEFAULT 0 COMMENT '阅读量',
+    like_num int unsigned NOT NULL DEFAULT 0 COMMENT '点赞量',
+    PRIMARY KEY(id),
+    KEY(biz)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS task_list
+(
+    id  bigint unsigned NOT NULL AUTO_INCREMENT,
+    content_url varchar(255) NOT NULL DEFAULT '',
+    is_load tinyint unsigned NOT NULL DEFAULT 0,
+    PRIMARY KEY(id),
+    UNIQUE KEY(content_url)
+) ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS news_tags
+(
+    id  bigint unsigned NOT NULL AUTO_INCREMENT,
+    content varchar(128) NOT NULL,
+    ctime   datetime NOT NULL DEFAULT '2016-01-01 00:00:00',
+    times   int unsigned NOT NULL DEFAULT 0,
+    PRIMARY KEY(id),
+    UNIQUE KEY(content)
+) ENGINE = InnoDB;
